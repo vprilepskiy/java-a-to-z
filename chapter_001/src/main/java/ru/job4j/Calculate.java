b3a62230635cce737dc760239b1637e60035d944
vprilepskiy <a89122285824@gmail.com>
2016-11-15 22:25:39 +0500

    add file

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package parsentev;
 
+/**
+ * Class Calculate решение задачи части 001 урок 1.
+ * @auhtor vprilepskiy
+ * @since 15.11.2016
+ * @version 1
+ */
 public class Calculate{
 
+	/**
+	 * Конструктор. Выводит сообщение в консоль "Hello World!!!"
+	 * @param args - args.
+	 */
 	public static void main (String [] args){
 		System.out.println("Hello World!!!");
 	}
