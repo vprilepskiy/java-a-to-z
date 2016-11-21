0341eb204e9d723865201aa24bb3b936e090a9cb
vprilepskiy <a89122285824@gmail.com>
2016-11-21 21:42:58 +0500

    update file

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -2,17 +2,17 @@ package ru.job4j;
 
 /**
  * Class Calculate решение задачи части 001 урок 1.
- * @auhtor vprilepskiy
+ * @author vprilepskiy
  * @since 15.11.2016
  * @version 1
  */
-public class Calculate{
+public class Calculate {
 
 	/**
 	 * Конструктор. Выводит сообщение в консоль "Hello World!!!"
 	 * @param args - args.
 	 */
-	public static void main (String [] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 	}
 
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -8,21 +8,19 @@ import static org.junit.Assert.assertThat;
 
 /**
  * Test.
- * 
- * @auhtor vprilepskiy
+ * @author vprilepskiy
  * @since 15.11.2016
  * @version 1
  */
  public class CalculateTest {
- 
 	/**
 	 * Test add.
 	 */
 	@Test
 	public void whenAddOneToOneThenTwo() {
-		ByteArrayOutputStream out = new ByteArrayOutputStream(); // для записи в него вывода с консоли
-		System.setOut(new PrintStream(out)); // заменяем системный поток вывода
-		Calculate.main(null); // вызов метода main
-		assertThat(out.toString(), is("Hello World!!!\r\n")); // делаем заключительную проверку результата вывода и ожидаемых данных.
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(out.toString(), is("Hello World!!!\r\n"));
 	}
  }
