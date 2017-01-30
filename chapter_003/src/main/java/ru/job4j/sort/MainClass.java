31960d3060901bf198a77c202870a5dce8a2c163
vprilepskiy <a89122285824@gmail.com>
2017-01-30 12:23:41 +0500

    Add file java in IntelliJ

--- chapter_003/src/main/java/ru/job4j/sort/MainClass.java
+++ chapter_003/src/main/java/ru/job4j/sort/MainClass.java
@@ -10,13 +10,13 @@ public class MainClass {
 
 
     public static void main(String[] a){
+        long startTime = System.currentTimeMillis();
 
-
-        String pathRead = "C:\\Downloads\\theme-1649.xml";
+        String pathRead = "C:\\Downloads\\bookRandom.txt";
         ReadBigFile readBigFile = new ReadBigFile(pathRead);
         Mapping mapping = new Mapping();
         int index = 0;
-        int sizeArray = 128;
+        int sizeArray = 65536;
         String[] pathFilesForMerged = new String[32768];
         do {
             // получаем массив содержащий {динна строки; номер байта начало строки}
@@ -32,11 +32,18 @@ public class MainClass {
             index++;
         } while (!readBigFile.readToEnd);
 
+        System.out.println((System.currentTimeMillis() - startTime) / 1000 / 60);
+        System.out.println("выполним слияние индексных файлов");
+
         // выполним слияние файлов
         String pathIndexMap = mapping.dualUnionElementsOfArray(pathFilesForMerged);
         //System.out.println(pathIndexMap);
 
+        System.out.println((System.currentTimeMillis() - startTime) / 1000 / 60);
+        System.out.println("Запишем по карте новый файл");
+
         WriteBigFile writeBigFile = new WriteBigFile(pathRead, pathIndexMap, pathRead + "SORT");
+        System.out.println((System.currentTimeMillis() - startTime) / 1000 / 60);
     }
 
 }
---
+++ chapter_003/src/test/java/ru/job4j/sort/Sonneteer.java
@@ -0,0 +1,67 @@
+package ru.job4j.sort;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * Created by VLADIMIR on 29.01.2017.
+ */
+public class Sonneteer {
+
+    /**
+     * Метод создаст текстовый файл размером около 4Гб.
+     *
+     * @param pathFile - путь куда сохранить файл.
+     */
+    public void writeDifferentWords(String pathFile) {
+        new File(pathFile).delete();
+        FileWriter fileWriter = null;
+        final String letters = "qwertyuiop asdfghjkl zxcvbnm";
+
+        try {
+            fileWriter = new FileWriter(pathFile, true);
+
+            final int maxNumberLettersInString = 200;
+//            final int maxNumberString = 40000000;
+            final int maxNumberString = 1000000;
+
+            // цикл строк
+            for (int countString = 0; countString < maxNumberString; countString++) {
+                int randomNumberLettersInString = this.getRandomNumber(maxNumberLettersInString);
+                // цикл букв.
+                for (int countLettersInString = 0; countLettersInString < randomNumberLettersInString; countLettersInString++) {
+                    int randomIndexLetter = this.getRandomNumber(letters.length());
+                    fileWriter.append(letters.toCharArray()[randomIndexLetter]);
+                }
+                fileWriter.append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                /*NONE*/
+            }
+        }
+    }
+
+    /**
+     * Генерирует случайное число.
+     * @param maxValue - максимальное значение.
+     * @return - случайное число.
+     */
+    private int getRandomNumber(int maxValue) {
+        return Math.abs(new Random().nextInt() % maxValue);
+    }
+
+    @Test
+    public void testWriteDifferentWords() {
+        this.writeDifferentWords("C:\\Downloads\\bookRandom.txt");
+    }
+}
