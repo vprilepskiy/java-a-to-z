f1e69811fd0b9b0d8f9c449feabc03de3ce6236c
vprilepskiy <a89122285824@gmail.com>
2017-04-26 09:32:01 +0500

    Add file java in IntelliJ

--- chapter_003.2/src/main/java/ru/job4j/manager/Actions.java
+++ chapter_003.2/src/main/java/ru/job4j/manager/Actions.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by VLADIMIR on 14.02.2017.
