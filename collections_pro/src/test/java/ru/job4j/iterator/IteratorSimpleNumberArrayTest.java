1d101e9f0490e536851bf85a6290bf16efd34f1d
vprilepskiy <a89122285824@gmail.com>
2017-05-25 20:34:42 +0500

    Add file java in IntelliJ

--- collections_pro/src/test/java/ru/job4j/iterator/IteratorSimpleNumberArrayTest.java
+++ collections_pro/src/test/java/ru/job4j/iterator/IteratorSimpleNumberArrayTest.java
@@ -11,12 +11,21 @@ public class IteratorSimpleNumberArrayTest {
 
     @Test
     public void whenSetNumberThenGetIsSimple(){
-        final int[] values = new int[] {};
+        final int[] values = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         IteratorSimpleNumberArray it = new IteratorSimpleNumberArray(values);
 
-        Assert.assertThat(it.isPrime(0), Is.is(false));
-        Assert.assertThat(it.isPrime(1), Is.is(false));
-        Assert.assertThat(it.isPrime(2), Is.is(true));
+
+
+        Assert.assertThat(it.isPrime(values[0]), Is.is(false));
+        Assert.assertThat(it.isPrime(values[1]), Is.is(false));
+        Assert.assertThat(it.isPrime(values[2]), Is.is(true));
+        Assert.assertThat(it.isPrime(values[3]), Is.is(true));
+        Assert.assertThat(it.isPrime(values[4]), Is.is(false));
+        Assert.assertThat(it.isPrime(values[5]), Is.is(true));
+        Assert.assertThat(it.isPrime(values[6]), Is.is(false));
+        Assert.assertThat(it.isPrime(values[7]), Is.is(true));
+        Assert.assertThat(it.isPrime(values[8]), Is.is(false));
+        Assert.assertThat(it.isPrime(values[9]), Is.is(false));
     }
 
 
