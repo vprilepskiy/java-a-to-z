47e10280ee5f4ee7b8131bff61340d62f1805046
vprilepskiy <a89122285824@gmail.com>
2017-05-30 14:44:07 +0500

    Add file java in IntelliJ

---
+++ collections_pro/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,69 @@
+package ru.job4j.generic;
+
+/**
+ * 5.2.1. Реализовать SimpleArray<T>.
+ * Доделать контейнер SimpleArray<T> добавить методы add, update, delete, get(int index);
+ *
+ * Created by VLADIMIR on 29.05.2017.
+ *
+ * @param <T> - Generic.
+ */
+public class SimpleArray<T> {
+
+    /**
+     * Массив для хранения объектов.
+     */
+    private Object[] objects;
+    /**
+     * Индекс последней свободной ячейки.
+     */
+    private int index = 0;
+
+    /**
+     * Конструктор.
+     * @param size - размер массива.
+     */
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+
+    /**
+     * Добавить элемент.
+     * @param value - значение.
+     */
+    public void add(T value) {
+        this.objects[index++] = value;
+    }
+
+
+    /**
+     * Обновить значение.
+     * @param position - номер ячейки.
+     * @param value - значение.
+     */
+    public void update(int position, T value) {
+        this.objects[position] = value;
+    }
+
+
+    /**
+     * Удалить элемент.
+     * @param position - номер ячейки.
+     */
+    public void delete(int position) {
+        this.objects[position] = null;
+    }
+
+
+    /**
+     * Получить значение.
+     * @param position - номер ячейки.
+     * @return - значение.
+     */
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+}
+
+
---
+++ collections_pro/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -0,0 +1,117 @@
+package ru.job4j.generic;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by VLADIMIR on 30.05.2017.
+ */
+public class SimpleArrayTest {
+
+    /**
+     * Test add. Type String.
+     */
+    @Test
+    public void whenCreateContainerShouldReturnTheSameType() {
+        final int size = 2;
+        SimpleArray<String> simpleArray = new SimpleArray<>(size);
+
+        simpleArray.add("first");
+        simpleArray.add("second");
+
+        Assert.assertThat(simpleArray.get(0), Is.is("first"));
+        Assert.assertThat(simpleArray.get(1), Is.is("second"));
+    }
+
+    /**
+     * Test add. Type Integer.
+     */
+    @Test
+    public void whenTypeIntShouldReturnInt() {
+        final int size = 1;
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(size);
+
+        simpleArray.add(1);
+
+        Assert.assertThat(simpleArray.get(0), Is.is(1));
+    }
+
+    /**
+     * Test update. Type String.
+     */
+    @Test
+    public void whenUpdateElement() {
+        final int size = 1;
+        SimpleArray<String> simpleArray = new SimpleArray<>(size);
+
+        simpleArray.add("test");
+        simpleArray.update(0, "update");
+
+        Assert.assertThat(simpleArray.get(0), Is.is("update"));
+    }
+
+    /**
+     * Test delete. Type String.
+     */
+    @Test
+    public void whenDeleteElement() {
+        final int size = 1;
+        SimpleArray<String> simpleArray = new SimpleArray<>(size);
+
+        simpleArray.add("test");
+        simpleArray.delete(0);
+
+        Assert.assertNull(simpleArray.get(0));
+    }
+
+}
+
+
+/**
+ * Job4j / Courses / Junior / Задания / 2. Пакет. Стандарт. / Часть 005. Collections. Pro. / 2. Generic /.
+ */
+class Examples {
+    /**
+     * Example. Принимает и возвращает объект. Выводит его в консоль.
+     * @param value - объект.
+     * @param <K> - тип объекта.
+     * @return - объект.
+     */
+    private <K> K print(K value) {
+        System.out.println(value);
+        return value;
+    }
+
+
+    /**
+     * Example. Метод может принимать аргументы типа Set и все наследованные от него.
+     * @param list - объект.
+     */
+    private void printUpper(SimpleArray<? extends Set> list) {
+    }
+
+    /**
+     * Example. Метод может принимать аргументы типа Set и все родительские объекты.
+     * @param list - объект.
+     */
+    private void printLower(SimpleArray<? super Set> list) {
+    }
+
+    /**
+     * В данном примере показано какие объекты можно использовать в generic.
+     * 1. Наследованный объект HashSet от Set.
+     * 2. Родительский объект Collection для Set.
+     */
+    private void aVoid() {
+        // использование наследованного объекта от Set.
+        this.printUpper(new SimpleArray<HashSet>(0));
+
+        // использование родительского объекта Set.
+        this.printLower(new SimpleArray<Collection>(0));
+    }
+}
