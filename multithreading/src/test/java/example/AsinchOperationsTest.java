c5c9a9e1fbd74c7b3df52b3a9727eb4c8054b2d5
vprilepskiy <a89122285824@gmail.com>
2017-09-18 12:49:19 +0500

    Commit.

--- multithreading/src/test/java/example/AsinchOperationsTest.java
+++ multithreading/src/test/java/example/AsinchOperationsTest.java
@@ -7,12 +7,9 @@ import org.junit.Test;
  * Created by VLADIMIR on 15.09.2017.
  */
 public class AsinchOperationsTest {
+
     @Test
     public void a() throws Exception {
-        new AsinchOperations().a();
-    }
-
-    public static void main(String[] args) {
         new AsinchOperations().a();
     }
 }
