f6b589757867895b498771386a3cf4c8404be1d2
vprilepskiy <a89122285824@gmail.com>
2017-11-27 10:27:11 +0500

    Commit.

--- Servlet_JSP/src/main/java/ru/job4j/store/UserStore.java
+++ Servlet_JSP/src/main/java/ru/job4j/store/UserStore.java
@@ -9,11 +9,10 @@ import java.util.List;
 
 /**
  * Created by VLADIMIR on 22.11.2017.
- *
+ * <p>
  * SINGLETON. Eager initialization.
  */
 public class UserStore {
-
 
     /**
      * Static class INSTANCE.
@@ -43,6 +42,12 @@ public class UserStore {
 
 
     /**
+     * Settings.
+     */
+    private final Settings settings = new Settings("store.properties");
+
+
+    /**
      * Set connection for DB.
      *
      * @param url      - URL.
@@ -51,7 +56,7 @@ public class UserStore {
      */
     public void setConnection(String url, String username, String password) {
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName(settings.getValue("db_driver_name"));
 
             if (this.connection == null) {
                 this.connection = DriverManager.getConnection(url, username, password);
@@ -80,111 +85,92 @@ public class UserStore {
 
 
     /**
-     * Manager for DB.
+     * Get all users.
+     *
+     * @return - users.
      */
-    private class Manager {
+    public List<User> get() {
+        final List<User> users = new LinkedList<>();
 
-        /**
-         * Select.
-         * @param sql - sql.
-         * @return
-         */
-        public List<User> getUsers(String sql) {
-            final List<User> users = new LinkedList<>();
+        try (Statement st = connection.createStatement();
+             ResultSet rs = st.executeQuery(settings.getValue("SQL_SELECT_ALL_USERS"))) {
 
-            try (ResultSet rs = connection.createStatement().executeQuery(sql)) {
-                while (rs.next()) {
-                    final String name = rs.getString("name");
-                    final String login = rs.getString("login");
-                    final String email = rs.getString("email");
-                    final String createDate = rs.getString("create_date");
-                    users.add(new User(name, login, email, createDate));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            while (rs.next()) {
+                final String name = rs.getString("name");
+                final String login = rs.getString("login");
+                final String email = rs.getString("email");
+                final String createDate = rs.getString("create_date");
+                users.add(new User(name, login, email, createDate));
             }
 
-            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
-
-        /**
-         * Update witch prepare arguments.
-         * @param prepareSQL - SQL.
-         * @param args - arguments.
-         * @return - number rows.
-         */
-        public int editUser(String prepareSQL, String[] args) {
-
-            int updateRows = 0;
-
-            try (PreparedStatement st = connection.prepareStatement(prepareSQL)) {
-                for (int i = 0; i < args.length; i++) {
-                    st.setString(i + 1, args[i]);
-                }
-
-                updateRows = st.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return updateRows;
-        }
+        return users;
     }
 
 
     /**
-     * Settings.
+     * Update witch prepare arguments.
+     *
+     * @param prepareSQL - SQL.
+     * @param args       - arguments.
+     * @return - number rows.
      */
-    private final Settings settings = new Settings("store.properties");
+    private int editUser(String prepareSQL, String[] args) {
 
-    /**
-     * Manager.
-     */
-    private final Manager dbManager = new Manager();
+        int updateRows = 0;
 
+        try (PreparedStatement st = connection.prepareStatement(prepareSQL)) {
+            for (int i = 0; i < args.length; i++) {
+                st.setString(i + 1, args[i]);
+            }
 
-    /**
-     * Get all users.
-     * @return - users.
-     */
-    public List<User> get() {
-        return this.dbManager.getUsers(settings.getValue("SQL_SELECT_ALL_USERS"));
+            updateRows = st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return updateRows;
     }
 
 
     /**
      * Add user.
+     *
      * @param user - user.
      * @return - number of added users
      */
     public int add(User user) {
         final String prepareSQL = settings.getValue("SQL_INSERT_USER");
         final String[] args = {user.getName(), user.getLogin(), user.getEmail(), user.getName()};
-        return this.dbManager.editUser(prepareSQL, args);
+        return this.editUser(prepareSQL, args);
     }
 
 
     /**
      * Edit user.
+     *
      * @param user - user.
      * @return - number of edited users.
      */
     public int edit(User user) {
         final String prepareSQL = settings.getValue("SQL_UPDATE_USER");
         final String[] args = {user.getLogin(), user.getEmail(), user.getName()};
-        return this.dbManager.editUser(prepareSQL, args);
+        return this.editUser(prepareSQL, args);
     }
 
 
     /**
      * Delete user.
+     *
      * @param user - user.
      * @return - number of remote users.
      */
     public int delete(User user) {
         final String prepareSQL = settings.getValue("SQL_DELETE_USER");
         final String[] args = {user.getName()};
-        return this.dbManager.editUser(prepareSQL, args);
+        return this.editUser(prepareSQL, args);
     }
 
 }
