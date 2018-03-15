39b0723f1498623b8bdb306717eac03b4aac65df
vprilepskiy <a89122285824@gmail.com>
2018-03-15 09:50:02 +0500

    Commit.

--- Hibernate_sale_cars/src/main/java/ru/job4j/view/servlet/MyItems.java
+++ Hibernate_sale_cars/src/main/java/ru/job4j/view/servlet/MyItems.java
@@ -25,6 +25,13 @@ import java.util.Set;
  */
 public class MyItems extends HttpServlet {
 
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/json");
