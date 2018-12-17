5f947642ae0d3f075416e10d2cff8c729a2a52a9
pvv <pvv@xrm.ru>
2018-12-17 14:16:49 +0500

    Изучение JAVA8 Lambda

--- StreamAPI/src/main/java/ru/prilepskiy/Lambda.java
+++ StreamAPI/src/main/java/ru/prilepskiy/Lambda.java
@@ -1,7 +1,15 @@
 package ru.prilepskiy;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lambda {
 
+    public static void main(String[] args) {
+        final List<String> strings = Arrays.asList("MacBook", "Apple", "Samsung", "Casio", "Sony", "LG", "IIyama");
 
+        strings.forEach(s -> System.out.println(s));
+        strings.forEach(System.out::println);
+    }
 
 }
