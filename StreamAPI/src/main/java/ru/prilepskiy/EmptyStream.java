3ee1033f99c4855bca107d2ecf8e56935980e14f
pvv <pvv@xrm.ru>
2019-03-04 14:30:30 +0500

    Изучение JAVA8 StreamAPI

--- StreamAPI/src/main/java/ru/prilepskiy/EmptyStream.java
+++ StreamAPI/src/main/java/ru/prilepskiy/EmptyStream.java
@@ -1,4 +1,16 @@
 package ru.prilepskiy;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class EmptyStream {
+
+    public static void main(String[] args) {
+
+        Set<Object> collect = Stream.empty().collect(Collectors.toSet());
+        System.out.println(collect.size());
+
+    }
+
 }
