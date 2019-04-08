8a7fbd94f3d45d3b604d5a80d7a2fd8ec6fa6fad
pvv <pvv@xrm.ru>
2019-04-08 16:57:08 +0500

    Task multithreading

--- Garbage_Collection/src/main/java/ru/prilepskiy/executor/ExecutorExample.java
+++ Garbage_Collection/src/main/java/ru/prilepskiy/executor/ExecutorExample.java
@@ -1,4 +1,39 @@
 package ru.prilepskiy.executor;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Пул потоков.
+ */
 public class ExecutorExample {
+
+    public static void main(String[] args) {
+        ExecutorService pool = null;
+        try {
+            pool = Executors.newFixedThreadPool(2);
+            pool.submit(new RunnableImpl());
+            pool.submit(new CallableImpl());
+        } finally {
+            pool.shutdown();
+        }
+    }
+
+
+    static class RunnableImpl implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Runnable");
+        }
+    }
+
+    static class CallableImpl implements Callable<Integer> {
+        @Override
+        public Integer call() throws Exception {
+            System.out.println("Callable");
+            return 1;
+        }
+    }
+
 }
--- Garbage_Collection/src/main/java/ru/prilepskiy/futures/CallableExample.java
+++ Garbage_Collection/src/main/java/ru/prilepskiy/futures/CallableExample.java
@@ -1,4 +1,33 @@
 package ru.prilepskiy.futures;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 public class CallableExample {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        final CallableImpl callable = new CallableImpl();
+        final FutureTask<Integer> task = new FutureTask<>(callable);
+
+        new Thread(task).start();
+        System.out.println("Выполнился?: " + task.isDone());
+        System.out.println("Ждать выполнения и запросить результат: " + task.get());
+        System.out.println("Ждать выполнения максимум 5мс. и запросить результат: " + task.get(5, TimeUnit.MILLISECONDS));
+        System.out.println("Выполнился?: " + task.isDone());
+    }
+
+    public static class CallableImpl implements Callable<Integer> {
+        @Override
+        public Integer call() throws Exception {
+            Integer result = 0;
+            for (int i = 0; i < 500_000_000; i++) {
+                result = i;
+            }
+            return result;
+        }
+    }
+
 }
