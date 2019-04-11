a789a00b0144eeb37b739d4581ae07d37766f660
pvv <pvv@xrm.ru>
2019-04-11 12:43:44 +0500

    Task multithreading 4.2

--- multithreading/src/main/java/wait/task2/ParallelSearch.java
+++ multithreading/src/main/java/wait/task2/ParallelSearch.java
@@ -1,4 +1,35 @@
 package wait.task2;
 
+import wait.task1.SimpleBlockingQueue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ParallelSearch {
+
+    public static void main(String[] args) {
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(2);
+        AtomicBoolean isAlive = new AtomicBoolean(true);
+
+        final Thread consumer = new Thread(() -> {
+                while (isAlive.get()) {
+                    System.out.println(queue.poll());
+                }
+            }
+        );
+
+        final Thread producer = new Thread(() -> {
+            for (int index = 0; index != 3; index++) {
+                try {
+                    queue.offer(index);
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            isAlive.set(false);
+        });
+
+        consumer.start();
+        producer.start();
+    }
 }
--- multithreading/src/main/java/wait/task2/package-info.java
+++ multithreading/src/main/java/wait/task2/package-info.java
@@ -1,1 +1,64 @@
 package wait.task2;
+/*
+*
+* 2. Обеспечить остановку потребителя. [#66825]
+  Петр Арсентьев,  17.08.18 13:31
+В этом задании нужно разработать механизм остановки потребителя, когда производитель закончил свою работу.
+
+Представим утилиту по поиску текста в файловой системе.
+
+Одна нить ищет файлы с подходящим именем. Вторая нить берет эти файлы и читает.
+
+Это схема хорошо описывается шаблон Producer Consumer. Однако есть один момент.
+
+Когда первая нить заканчивает свою работу, потребители переходят в режим wait.
+
+И утилита работает вечно.
+
+Давайте составим упрощенный код такой программы.
+
+package ru.job4j.buffer;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
+
+Если мы запустим этот код. то на консоли мы увидим. что нить производитель закончила работу, а нить потребитель продолжает ждать событий.
+Ваша задача изменить код, так. что бы потребитель завершал свою работу.
+Код нужно изменить в методе main.
+*/
