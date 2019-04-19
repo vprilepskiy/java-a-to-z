9cb7cc8605571d0549d8fca8349e219535ed0d7e
pvv <pvv@xrm.ru>
2019-04-19 11:10:51 +0500

    fix after review Task multithreading 4.3

--- multithreading/src/test/java/wait/task1/SimpleBlockingQueueTest.java
+++ multithreading/src/test/java/wait/task1/SimpleBlockingQueueTest.java
@@ -3,31 +3,16 @@ package wait.task1;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SimpleBlockingQueueTest {
 
-    SimpleBlockingQueue<String> queue = new SimpleBlockingQueue<String>(2);
+    private final SimpleBlockingQueue<String> queue = new SimpleBlockingQueue<>(2);
 
     @Test
     public void test() throws InterruptedException {
+        queue.offer("1");
+        queue.offer("2");
 
-        final Thread producer = new Thread(() -> {
-            try {
-                queue.offer("1");
-                queue.offer("2");
-                queue.offer("3");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        final Thread customer = new Thread(() -> {
-            Assert.assertEquals(queue.poll(), "1");
-        });
-
-        producer.start();
-        producer.join(1000);
-        customer.start();
+        Assert.assertEquals(queue.poll(), "1");
+        Assert.assertEquals(queue.poll(), "2");
     }
 }
