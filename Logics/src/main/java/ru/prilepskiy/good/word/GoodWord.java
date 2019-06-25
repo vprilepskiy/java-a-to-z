7bd1856d01cd558fb171aba8033a20260deca1e1
pvv <pvv@xrm.ru>
2019-06-25 10:00:23 +0500

    fix after review

--- Logics/src/main/java/ru/prilepskiy/good/word/GoodWord.java
+++ Logics/src/main/java/ru/prilepskiy/good/word/GoodWord.java
@@ -2,27 +2,26 @@ package ru.prilepskiy.good.word;
 
 public class GoodWord {
 
-    public Word convert(Word w) {
-        final StringBuilder sb = new StringBuilder(w.getS());
+    public String convert(String word) {
+        final StringBuilder sb = new StringBuilder(word);
 
-        if (1 > w.getK() || w.getK() > 2 * Math.pow(10, 5)) {
+        if (1 > word.length() || word.length() > 2 * Math.pow(10, 5)) {
             throw new IllegalArgumentException("Количество символов в строке не соответствует числу n (1≤n≤2*10^5)");
-        } else if (w.getK() != w.getS().length()) {
-            throw new IllegalArgumentException("Число символов в строке не соответствует числу n.");
-        } else {
-            for (int i = 0; i < sb.length(); i = i + 2) {
-                while (true) {
-                    if (sb.length() <= i + 1) {
-                        sb.deleteCharAt(i);
-                        break;
-                    } else if (sb.charAt(i) == sb.charAt(i + 1)) {
-                        sb.deleteCharAt(i);
-                    } else {
-                        break;
-                    }
+        }
+
+        for (int i = 0; i < sb.length(); i = i + 2) {
+            while (true) {
+                if (sb.length() <= i + 1) {
+                    sb.deleteCharAt(i);
+                    break;
+                } else if (sb.charAt(i) == sb.charAt(i + 1)) {
+                    sb.deleteCharAt(i);
+                } else {
+                    break;
                 }
             }
         }
-        return new Word(w.getS().length() - sb.length(), sb.toString());
+
+        return sb.toString();
     }
 }
--- Logics/src/test/java/ru/prilepskiy/good/word/GoodWordTest.java
+++ Logics/src/test/java/ru/prilepskiy/good/word/GoodWordTest.java
@@ -9,17 +9,14 @@ import java.util.Map;
 
 public class GoodWordTest {
 
-    final Map<Word, Word> dataAndExpectedResult = new HashMap();
-    final Map<Word, Word> failDataAndExpectedResult = new HashMap();
+    final Map<String, String> dataAndExpectedResult = new HashMap();
 
     @Before
     public void addTestData() {
-        this.dataAndExpectedResult.put(new Word(4, "good"), new Word(0, "good"));
-        this.dataAndExpectedResult.put(new Word(4, "aabc"), new Word(2, "ab"));
-        this.dataAndExpectedResult.put(new Word(3, "aaa"), new Word(3, ""));
-
-        this.failDataAndExpectedResult.put(new Word(0, ""), null);
-        this.failDataAndExpectedResult.put(new Word(1, "ab"), null);
+        this.dataAndExpectedResult.put("good", "good");
+        this.dataAndExpectedResult.put("aabc", "ab");
+        this.dataAndExpectedResult.put("aaa", "");
+        this.dataAndExpectedResult.put("ab", "ab");
     }
 
     @Test
@@ -33,6 +30,6 @@ public class GoodWordTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void convertFail() {
-        failDataAndExpectedResult.forEach((data, expect) -> new GoodWord().convert(data));
+        new GoodWord().convert("");
     }
 }
