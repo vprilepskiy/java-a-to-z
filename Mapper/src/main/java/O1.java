26d1930a9508d040da41e11c9b7d526eae6479a2
pvv <pvv@xrm.ru>
2019-07-19 10:54:58 +0500

    Mapper Dozer

--- Mapper/src/main/java/O1.java
+++ Mapper/src/main/java/O1.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
-
 public class O1 {
+
+    public O1(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    private String field1;
+    private String field2;
+    private String field3;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+
+    @Override
+    public String toString() {
+        return "O1{" +
+            "field1='" + field1 + '\'' +
+            ", field2='" + field2 + '\'' +
+            ", field3='" + field3 + '\'' +
+            '}';
+    }
 }
--- Mapper/src/main/java/O2.java
+++ Mapper/src/main/java/O2.java
@@ -1,14 +1,16 @@
-public class O1 {
+public class O2 {
 
-    public O1(String field1, String field2, String field3) {
+    public O2(String field1, String field2, String field3, String field4) {
         this.field1 = field1;
         this.field2 = field2;
         this.field3 = field3;
+        this.field4 = field4;
     }
 
     private String field1;
     private String field2;
     private String field3;
+    private String field4;
 
     public String getField1() {
         return field1;
@@ -32,5 +34,23 @@ public class O1 {
 
     public void setField3(String field3) {
         this.field3 = field3;
+    }
+
+    public String getField4() {
+        return field4;
+    }
+
+    public void setField4(String field4) {
+        this.field4 = field4;
+    }
+
+    @Override
+    public String toString() {
+        return "O2{" +
+            "field1='" + field1 + '\'' +
+            ", field2='" + field2 + '\'' +
+            ", field3='" + field3 + '\'' +
+            ", field4='" + field4 + '\'' +
+            '}';
     }
 }
