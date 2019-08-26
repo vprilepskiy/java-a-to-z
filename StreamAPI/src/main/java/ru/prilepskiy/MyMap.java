62b2910c63d366c283b5f72935a2ce115371d223
pvv <pvv@xrm.ru>
2019-08-26 08:59:13 +0500

    map merge example

--- StreamAPI/src/main/java/ru/prilepskiy/MyMap.java
+++ StreamAPI/src/main/java/ru/prilepskiy/MyMap.java
@@ -1,4 +1,18 @@
 package ru.prilepskiy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyMap {
+    public static void main(String[] args) {
+        Map<String, Boolean> map = new HashMap<>();
+
+        map.merge("1", Boolean.FALSE, (v1, v2) -> v1 || v2);
+        map.merge("1", Boolean.FALSE, (v1, v2) -> v1 || v2);
+        map.merge("2", Boolean.FALSE, (v1, v2) -> v1 || v2);
+        map.merge("2", Boolean.TRUE, (v1, v2) -> v1 || v2);
+        map.merge("2", Boolean.FALSE, (v1, v2) -> v1 || v2);
+
+        System.out.println(map);
+    }
 }
